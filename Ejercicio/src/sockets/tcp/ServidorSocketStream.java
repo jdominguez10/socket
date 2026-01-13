c2e65bd320298dc64bf3628ba78809dc6c92a756
Pablo <jpdmayo1986@gmail.com>
2026-01-13 14:00:28 +0100

    agregado lectura de total buffer

--- Ejercicio/src/sockets/tcp/ServidorSocketStream.java
+++ Ejercicio/src/sockets/tcp/ServidorSocketStream.java
@@ -9,35 +9,48 @@ import java.net.Socket;
 public class ServidorSocketStream {
 
 	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		Socket newSocket = null;
+		
 		try {
-			ServerSocket serverSocket = new ServerSocket();
+			serverSocket = new ServerSocket();
 			InetSocketAddress addr = new InetSocketAddress("0.0.0.0", 5555);
 			serverSocket.bind(addr);
 			
 			System.out.print("Servidor aceptando:" + addr.getHostName());
-			Socket newSocket = serverSocket.accept();
+			newSocket = serverSocket.accept();
 
 			InputStream is = newSocket.getInputStream();
 			
 			byte[] buffer = new byte[25];
-			is.read(buffer);
+			int leidos = is.read(buffer);
+			String recibido = new String(buffer, 0, leidos); 
 			
 			//System.out.println("Mensaje recibido: " + new String(mensaje));
-			analizaMensaje(buffer);
+			analizaMensaje(recibido);
 			
-			newSocket.close();
-			serverSocket.close();
-			
+		
 		}catch(IOException ex) {
 			ex.printStackTrace();
+		}finally {
+			try {
+				if(newSocket!=null) {
+					newSocket.close();
+				}
+				
+				if(serverSocket != null)
+					serverSocket.close();
+			} catch (IOException e) {
+				System.err.print(e.toString());
+			}
 		}
 
 	}
 
-	public static void analizaMensaje(byte[] mensaje) {
-		String msn = (new String(mensaje)).trim();
-		int tam = msn.length();
+	public static void analizaMensaje(String recibido) {
+	
+		int tam = recibido.length();
 		System.out.println("\nTotal: "+ tam);
-		System.out.println("\nMensaje: "+ msn);
+		System.out.println("\nMensaje: "+ recibido);
 	}
 }
